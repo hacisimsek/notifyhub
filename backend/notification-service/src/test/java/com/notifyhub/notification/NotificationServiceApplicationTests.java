635104944401efe19cb0f7256abbe2be8db4cc16
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2026-05-09 10:38:43 +0300

    test: avoid random ports in context smoke tests

--- backend/notification-service/src/test/java/com/notifyhub/notification/NotificationServiceApplicationTests.java
+++ backend/notification-service/src/test/java/com/notifyhub/notification/NotificationServiceApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class NotificationServiceApplicationTests {
 
     @Test
